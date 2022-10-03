e7e8a109d666c00ae1676d79e11895da9f71c480
Joanne Santhosh <joanne.santhosh@mths.ca>
2022-10-03 15:23:42 +0000

    update

---
+++ cost_of_pizza.cpp
@@ -0,0 +1,31 @@
+// Copyright (c) 2022 Joanne Santhosh All rights reserved
+//
+// Created by: Joanne Santhosh
+// Created on: Sept 2022
+// This program calculates the cost of a pizza 
+
+#include <cmath>
+#include <iostream>
+
+int main() {
+    // This function calculates the cost of a pizza
+    const float RENT = 1;
+    const float LABOUR = 0.75;
+    const float COST_PER_INCH = 0.5;
+    const float HST = 0.13;
+
+    float diameter 
+
+    // input
+    std::cout << "Enter the radius of the circle (mm): ";
+    std::cin >> radiusOfCircle;
+
+    // process
+    CircumferenceOfCircle = TAU * radiusOfCircle;
+
+    // output
+    std::cout << "" << std::endl;
+    std::cout << "The circumference is " << CircumferenceOfCircle;
+    std::cout << " mm." << std::endl;
+    std::cout << "\nDone." << std::endl;
+}
